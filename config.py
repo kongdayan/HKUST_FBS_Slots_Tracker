6e2d38c7fde1088eab8898c54025988c8c2039ee
kongdayan <kongwenyancn@gmail.com>
2023-10-30 16:13:10 +0800

    add HKUSTStaff,FBS,USThing fetch config

--- config.py
+++ config.py
@@ -19,6 +19,46 @@ data_config = {
             "start_date={startdate}&"
             "end_date={enddate}"
         ),
+        "methord": "GET",
+        "User-Agent": "",
+        "Authorization": "",
+    },
+    "HKUSTStaff": {
+        "FETCH_URL": (
+            "https://w5.ab.ust.hk/msstf/app/fbs/facilitytimeslot?"
+            "facilityID={id}&"
+            "userType={userType}&"
+            "ustID={ustID}&"
+            "startDate={startDate}&"
+            "endDate={endDate}"
+        ),
+        "methord": "GET",
+        "User-Agent": "",
+        "Authorization": "",
+    },
+    "FBS": {
+        "FETCH_URL": ("https://w6.ab.ust.hk/fbs_user/bin2/Timetable"),
+        "methord": "POST",
+        "User-Agent": "",
+        "Authorization": "",
+        "body": {
+            "location": "",
+            "facility_group": "",
+            "court": "",
+            "date": "",
+            "JSEnable": "",
+            "numChecked": "",
+        },
+    },
+    "USThing": {
+        "FETCH_URL": (
+            "https://booking.api.usthing.xyz/categories/fbs/badminton?"
+            "date={date}"
+            "slots_after={slots_after}"
+            "slots_before={slots_before}"
+            "group_slots=True"
+        ),
+        "methord": "GET",
         "User-Agent": "",
         "Authorization": "",
     },
